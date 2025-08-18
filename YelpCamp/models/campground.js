fdf5073023119ad0f8ba828bc423edb5825dd50f
Koki Ishizuka <koishizu@microsoft.com>
2025-08-18 17:22:21 +0900

    add CRU(D) to camp app

---
+++ YelpCamp/models/campground.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const campgroundSchema = new Schema({
+  title: String,
+  image: String,
+  description: String,
+  location: String
+});
+
+// 以下のモデルを作成すると、campgrounds コレクションが自動的に作成されます。
+// module.exports によって、他のファイルからこのモデルをインポートできるようになります。
+module.exports = mongoose.model('Campground', campgroundSchema);
