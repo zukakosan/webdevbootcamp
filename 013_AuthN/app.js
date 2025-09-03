6da43c989e9d9b88c12a6844f349f660059a1ce6
zukakosan <shizhong.hangxi@gmail.com>
2025-09-03 22:26:06 +0900

    user login state managed by req.session.user_id/static func to get rid of logics from route handler

--- 013_AuthN/app.js
+++ 013_AuthN/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 // connect to mongoose
 mongoose.connect('mongodb://localhost:27017/authDB', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
@@ -13,13 +15,34 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(flash());
+
+const requireLogin = (req, res, next) => {
+    if (req.session && req.session.user_id) {
+        next();
+    } else {
+        return res.redirect('/login');
+    }
+};
 
 app.get('/register', (req, res) => {
     res.render('register');
 });
 
 app.get('/login', (req, res) => {
-    res.render('login');
+    const error = req.flash('error')[0]; // 存在しなければ undefined
+    res.render('login', { error });
+});
+
+app.post('/logout',(req,res)=>{
+    // req.session.user_id = null;
+    req.session.destroy();
+    res.redirect('/login');
 });
 
 app.get('/error', (req, res) => {
@@ -29,30 +52,33 @@ app.get('/error', (req, res) => {
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     // findById が使えないため findOne を使用
-    const user = await User.findOne({ username });
-    if (user && await bcrypt.compare(password, user.password)) {
-        res.cookie('authenticity', true); // Set a cookie
+    // const user = await User.findOne({ username });
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        // ログイン状態とユーザIDを同時に管理するために user._id をセッションに保存
+        req.session.user_id = foundUser._id;
         res.redirect('/secret');
     } else {
-        res.redirect('/error');
+        req.flash('error', 'Invalid username or password');
+        res.redirect('/login');
     }
 });
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, password: hashedPassword });
+    const user = new User({ username, password});
     await user.save();
-    res.redirect('/login');
+    req.session.user_id = user._id;
+    res.redirect('/secret');
 })
 
-app.get('/secret', (req, res) => {
-    console.log(req.cookies);
-    if (req.cookies && req.cookies.authenticity) {
-        res.send("This is the secret page!");
-    } else {
-        res.redirect('/error');
-    }
+app.get('/secret', requireLogin, (req, res) => {
+    console.log(req.session);
+    res.render("secret", { user: req.session.user_id });
+});
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send('top secret!!!!!');
 });
 
 app.listen(3000, () => {
--- 013_AuthN/models/user.js
+++ 013_AuthN/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     username:{
         type: String,
@@ -9,6 +10,27 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.statics.findAndValidate = async function(username, password) {
+    const user = await this.findOne({ username });
+    if (user) {
+        const isValid = await bcrypt.compare(password, user.password);
+        // ユーザが存在し、パスワードが一致する場合はユーザ情報を返す
+        // 一致しない場合は false を返す
+        // ユーザオブジェクト　か　false という選択肢なのが少し注意
+        return isValid ? user : false;
+    }
+    return false;
+};
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        // パスワードが変更された時だけハッシュ化する
+        // この条件がないと、パスワード以外のユーザ情報を更新してもパスワードの再ハッシュ化が走ってしまう
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
