f8a48d8bdde26e5fd1490d018b4e7340dfc98ab8
zukakosan <shizhong.hangxi@gmail.com>
2025-09-12 09:32:09 +0900

    sanitize

--- 000_YelpCamp/app.js
+++ 000_YelpCamp/app.js
@@ -19,7 +19,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const userRoutes = require('./routes/users');
-
+const mongoSanitize = require('express-mongo-sanitize');
+const bodyParser = require('body-parser');
 // connect to mongoose
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -48,13 +49,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodoverride('_method'));
 // ログ用ミドルウェア
 app.use(morgan('dev'));
-
+app.use(express.json());
+// app.use(mongoSanitize({
+//     replaceWith: '_'
+// }));
 const sessionConfig = {
+    name: 'sessionid',
     secret: 'mysecret',
     resave: false,
     saveUninitialized: true,
     cookie:{
         httpOnly: true,
+        // secure: true, // https のときのみ cookie を送信する
         maxAge: 1000*60*60*24*7
     }
 };
@@ -73,6 +79,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
--- 000_YelpCamp/schemas.js
+++ 000_YelpCamp/schemas.js
@@ -1,11 +1,34 @@
-const joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const joi = BaseJoi.extend(extension);
 
 module.exports.campgroundSchema = joi.object({
     campground: joi.object({
-        title: joi.string().required(),
+        title: joi.string().required().escapeHTML(),
         image: joi.string().optional().allow(''),
-        description: joi.string().required(),
-        location: joi.string().required(),
+        description: joi.string().required().escapeHTML(),
+        location: joi.string().required().escapeHTML(),
         price: joi.number().min(0).required()
     }).required(),
     deleteImage: joi.string().optional().allow('')
@@ -13,7 +36,7 @@ module.exports.campgroundSchema = joi.object({
 
 module.exports.reviewSchema = joi.object({
     review: joi.object({
-        body: joi.string().allow('').optional(),
+        body: joi.string().allow('').optional().escapeHTML(),
         rating: joi.number().min(1).max(5).required()
     }).required()
 })
